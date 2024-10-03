0fd4f1777ddb1b74a055dd41f12fd2744dcbe826
uvsq22106020 <amira.hamad@ens.uvsq.fr>
2024-10-03 15:17:22 +0200

    toute la question 2

--- fraction.java
+++ fraction.java
@@ -1,7 +1,22 @@
-public class fraction {
+public class Fraction {
     private int numerateur;
     private int denominateur;
     public String toString() {
-        return "je suis une fraction !";
+        return " je suis une fraction avec comme numerateur :" + numerateur + "et comme denominateur" + denominateur;
     }    
+    public Fraction(int numerateur, int denominateur) {
+        if (denominateur == 0) {
+            throw new IllegalArgumentException("Le denominateur ne peut pas etre nul.");
+        }
+        this.numerateur = numerateur;
+        this.denominateur = denominateur;
+    }
+   // Constructeur avec juste le numérateur (dénominateur égal à 1)
+   public Fraction(int numerateur) {
+    this(numerateur, 1);
+    }
+    // Constructeur sans argument (numérateur égal 0 et dénominateur égal à 1)
+    public Fraction() {
+        this(0, 1);
+    }
 }
--- main.java
+++ main.java
@@ -1,7 +1,11 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        fraction fraction = new fraction();
+        Fraction fraction = new Fraction(11,7);
+        Fraction fraction1 = new Fraction(2);
+        Fraction fraction2 = new Fraction();
         System.out.println(fraction.toString());
+        System.out.println(fraction1.toString());
+        System.out.println(fraction2.toString());
     }
 }
 
